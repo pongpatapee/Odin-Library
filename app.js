32ab9a882663aa2179449b10bbf73a1ae72266ef
pongpatapee <pongpatapee1021@gmail.com>
2022-01-12 23:55:08 -0500

    got all core functionality working

--- app.js
+++ app.js
@@ -23,6 +23,16 @@ function closeForm() {
     bookForm.style.display = 'none';
 }
 
+function toggleHasRead(e) {
+    if(e.target.className == "has-read") {
+        e.target.className = "has-not-read";
+        e.target.innerText = "Not Read";
+    } else {
+        e.target.className = "has-read";
+        e.target.innerText = "Read";
+    }
+}
+
 function createBook() {
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
@@ -34,7 +44,36 @@ function createBook() {
 
 function createBookDiv(book) {
     let bookDiv = document.createElement('div');
-    bookDiv.innerText = book.info();
+    let title = document.createElement('h3');
+    let author = document.createElement('h3');
+    let pages = document.createElement('h3');
+    let hasReadBtn = document.createElement('button');
+    let rmvBtn = document.createElement('button');
+
+    title.innerText = `Title: ${book.title}`;
+    author.innerText = `Author: ${book.author}`;
+    pages.innerText = `${book.pages} pages`;
+
+    if(book.hasRead) {
+        hasReadBtn.classList.add('has-read');
+        hasReadBtn.innerText = "Read";
+    } else {
+        hasReadBtn.classList.add('has-not-read');
+        hasReadBtn.innerText = "Not Read";
+    }
+
+    hasReadBtn.addEventListener('click', toggleHasRead);
+
+    rmvBtn.classList.add('rmv-btn');
+    rmvBtn.innerText = 'Remove';
+    rmvBtn.addEventListener('click', removeBook);
+
+    bookDiv.appendChild(title);
+    bookDiv.appendChild(author);
+    bookDiv.appendChild(pages);
+    bookDiv.appendChild(hasReadBtn);
+    bookDiv.appendChild(rmvBtn);
+    bookDiv.Object = book;
     contentDiv.appendChild(bookDiv);
 }
 
@@ -48,25 +87,31 @@ function resetForm() {
 function addBook(e) {
     console.log(e.target);
     let newBook = createBook();
+    newBook.index = library.length;
     library.push(newBook);
-    createBookDiv(newBook);
+    createBookDiv(newBook, newBook.index);
     resetForm();
     closeForm();
+    console.log(library);
 }
 
+function removeBook(e) {
+    //horrible but it works (fix it later)
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+
+    //remove from library
+    bookObj = e.target.parentNode.Object
+    let index = library.indexOf(bookObj);
+    if(index > -1){
+        library.splice(index, 1);
+    }
+    console.log(`${bookObj.title} book has been removed`);
+    console.log(library);
+}
 
 const bookForm = document.getElementById('new-book-form');
 const contentDiv = document.querySelector('.content');
 const library = [];
 
-// // populate for testing 
-// library[0] = new Book("Wonder", "wonder's author", 305, true);
-// library[1] = new Book("One piece", "eichiro oda", 1036, true);
-// library[2] = new Book("Love is war", "i for got", 232, false);
-// //
-
-// library.forEach((book) => {
-//     createBookDiv(book);
-// });
 
 bookForm.addEventListener('submit', addBook);
